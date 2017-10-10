71b2185ce87a65c56d2b9e9cbda5920254ec6ac4
Didier Wenzek <didier.wenzek@acidalie.com>
2017-10-10 14:59:28 +0200

    Using type constraint to simplify relation type.

--- eval/data-module/capability.ml
+++ eval/data-module/capability.ml
@@ -107,3 +107,95 @@ module type Plan = sig
   val _inv_map:  ('a,'c) var -> ('a,'b, Cap.t,Cap.t,Cap.yes,Cap.t) relation -> ('c,'a,'d) injector -> ('c,'b) extractor -> 'c value -> 'd value
   val _generate: ('a,'c) var -> ('a,'b, Cap.yes,Cap.t,Cap.t,Cap.t) relation -> ('c,'a,'d) injector ->  ('d,'b,'e) injector -> 'c value -> 'e value
 end
+
+module type S_bis = sig
+  module Gen = Cap
+  module Map = Cap
+  module Inv = Cap
+  module Chk = Cap
+
+  type ('gen_cap,'map_cap,'inv_cap,'chk_cap) rel_cap
+  type gen_cap = (Gen.yes,Map.t,Inv.t,Chk.t) rel_cap
+  type map_cap = (Gen.t,Map.yes,Inv.t,Chk.t) rel_cap
+  type inv_cap = (Gen.t,Map.t,Inv.yes,Chk.t) rel_cap
+  type chk_cap = (Gen.t,Map.t,Inv.t,Chk.yes) rel_cap
+  
+  type ('a,'b,'rel_cap) relation
+    constraint 'rel_cap = ('gen_cap,'map_cap,'inv_cap,'chk_cap) rel_cap
+
+  type 'a value
+
+  val generate: ('a,'b, gen_cap) relation -> ('c,'a,'d) injector ->  ('d,'b,'e) injector -> 'c value -> 'e value
+  val map: ('a,'b, map_cap) relation -> ('c,'a) extractor -> ('c,'b,'d) injector -> 'c value -> 'd value
+  val inv_map: ('a,'b, inv_cap) relation -> ('c,'a,'d) injector -> ('c,'b) extractor -> 'c value -> 'd value
+  val filter: ('a,'b, chk_cap) relation -> ('c,'a) extractor -> ('c,'b) extractor -> 'c value -> 'c value
+
+  val rel_of_fun: ('a -> 'b) -> ('a,'b, (Gen.no,Map.yes,Inv.no,Chk.yes) rel_cap) relation
+  val inverse: ('a,'b, ('c,'d,'e,'f) rel_cap) relation -> ('b,'a, ('c,'e,'d,'f) rel_cap) relation
+end
+
+module Impl_bis(Dataset: Dataset.S) : S_bis = struct
+
+  module Gen = Cap
+  module Map = Cap
+  module Inv = Cap
+  module Chk = Cap
+
+  type 'a dataset = 'a Dataset.t
+  type 'a value = 'a dataset
+
+  type ('gen_cap,'map_cap,'inv_cap,'chk_cap) rel_cap = C
+  type gen_cap = (Gen.yes,Map.t,Inv.t,Chk.t) rel_cap
+  type map_cap = (Gen.t,Map.yes,Inv.t,Chk.t) rel_cap
+  type inv_cap = (Gen.t,Map.t,Inv.yes,Chk.t) rel_cap
+  type chk_cap = (Gen.t,Map.t,Inv.t,Chk.yes) rel_cap
+
+  type ('a,'b, 'rel_cap) relation = {
+    gen: (('a*'b)  dataset, 'gen_cap) Cap.impl;
+    map: ('a -> 'b dataset, 'map_cap) Cap.impl;
+    inv: ('b -> 'a dataset, 'inv_cap) Cap.impl;
+    chk: ('a -> 'b -> bool, 'chk_cap) Cap.impl;
+  }
+  constraint 'rel_cap = ('gen_cap,'map_cap,'inv_cap,'chk_cap) rel_cap
+
+  let filter rel =
+    let chk = Cap.get rel.chk in
+    fun get_a get_b -> Dataset.filter (fun c -> chk (get_a c) (get_b c))
+
+  let map rel =
+    let map = Cap.get rel.map in
+    fun get_a set_b -> Dataset.flatmap (fun c -> map (get_a c) |> Dataset.map (set_b c))
+
+  let inv_map rel =
+    let inv = Cap.get rel.inv in
+    fun set_a get_b -> Dataset.flatmap (fun c -> inv (get_b c) |> Dataset.map (set_a c))
+
+  let generate rel =
+    let gen = Cap.get rel.gen in
+    fun set_a set_b ->
+      let pair c (a,b) =
+        let d = set_a c a 
+        in set_b d b
+      in
+      Dataset.flatmap (fun c -> Dataset.map (pair c) gen)
+
+  let rel = {
+    gen = Cap.NoCap;
+    map = Cap.NoCap;
+    inv = Cap.NoCap;
+    chk = Cap.NoCap;
+  }
+
+  let rel_of_fun f = {
+    rel with
+    map = Cap.Cap (fun x -> Dataset.singleton (f x));
+    chk = Cap.Cap (fun x y -> f x = y);
+  }
+
+  let inverse rel = {
+    gen = Cap.map (Dataset.map Util.swap_pair) rel.gen;
+    map = rel.inv;
+    inv = rel.map;
+    chk = Cap.map Util.swap rel.chk;
+  }
+end
