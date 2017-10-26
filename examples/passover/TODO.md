acbcea35b6f21787c4bbb5695754aece1c40d358
Didier Wenzek <didier.wenzek@acidalie.com>
2017-10-26 15:14:10 +0200

    Add partition argument to passover main.
    All topics must have the same count of partitions.
    Each process instance is assigned a specific partition.
    Exactly one process must run for each partition.

--- examples/passover/TODO.md
+++ examples/passover/TODO.md
@@ -1,5 +1,9 @@
 * Add kafka topic as a way to log consumed messages.
-* Add partition parameter to passover main.
 * A message must be produced to the right partition.
 * Log parse errors
 * The most recent position event must be registered, no the last one. 
+* Each instance of a runner must have its own set of databases
+* Each instance of a runner must have its own set of state files
+* Each instance of a runner must have its own set of error files
+* Fix error: Kyoto.Error("opening the destination failed")
+* Implement an event generator.
--- examples/passover/passover.ml
+++ examples/passover/passover.ml
@@ -1,14 +1,46 @@
+open Cmdliner
 open Message
 open Series
 open Util
 module Dataset = Incremental_value
 
-let host = "localhost"
+let info =
+  let doc = "Run PassOver continuous queries." in
+  let man = [
+    `S "DESCRIPTION";
+    `P "$(tname) PARTITION PARTITION_COUNT
 
-let consume_topic topic partition =
+        Continuously consumes visitor events,
+        aggregates visitor data
+        and produces visitor alerts.";
+  ] in
+  Term.info "passover" ~doc ~man
+
+let partition =
+  let doc = "partition this instance will consume (for all Kafka topics)" in
+  Arg.(required & pos 0 (some int) None & info [] ~docv:"PARTITION" ~doc)
+
+let partition_count =
+  let doc = "count of partitions (for all Kafka topics)" in
+  Arg.(required & pos 1 (some int) None & info [] ~docv:"PARTITION_COUNT" ~doc)
+
+let kafka_host =
+  let doc = "Connection string to the Kafka cluster." in
+  Arg.(value & opt string "localhost" & info ["k"; "kafka"] ~docv:"KAFKA_CLUSTER" ~doc)
+
+type cluster_info = {
+  kafka_host: string;
+  partition: int;
+  partition_count: int;
+}
+
+let consume_topic cluster topic =
+  let host = cluster.kafka_host in
+  let partition = cluster.partition in
   KafkaStore.Source.kafka_partition ~host ~topic ~partition
 
-let produce_topic topic =
+let produce_topic cluster topic =
+  let host = cluster.kafka_host in
   Dataset.log (KafkaStore.Sink.log_to_topic ~host ~topic)
 
 let store_kyoto_plain =
@@ -38,29 +70,39 @@ let most_recent =      (* FIXME: should take the most recent, not the last provi
     full_check = None;
   }
 
-let decode_position_messages partition =
-  consume_topic "passover.positions.messages" partition
+let decode_position_messages cluster =
+  consume_topic cluster "passover.positions.messages"
   |> Dataset.map position_of_json
   |> Dataset.filter_map Option.of_result
   |> Dataset.map json_of_position
   |> Dataset.filter_map Option.of_result
-  |> produce_topic "passover.positions.events"  (* FIXME: should assign a partition *)
+  |> produce_topic cluster "passover.positions.events"  (* FIXME: should assign a partition *)
   |> Dataset.run "/tmp/" "decode_position_messages"
 
-let update_current_visitor_room partition =
-  consume_topic "passover.positions.events" partition
+let update_current_visitor_room cluster =
+  consume_topic cluster "passover.positions.events"
   |> Dataset.map position_of_json
   |> Dataset.filter_map Option.of_result
   |> Dataset.group visitor_of_event room_of_event most_recent
   |> store_kyoto_plain "visitor_room.kct" 
   |> Dataset.run "/tmp/" "update_current_visitor_room"
 
-let main () =
-  let partition = 1 in
+let runner kafka_host partition partition_count =
+  let cluster =
+    { kafka_host; partition; partition_count; }
+  in
   Lwt_main.run (Lwt.join [
-    decode_position_messages partition;
-    update_current_visitor_room partition;
+    decode_position_messages cluster;
+    update_current_visitor_room cluster;
   ])
+
+let runner_t =
+  Term.(pure runner $ kafka_host $ partition $ partition_count)
+
+let main () =
+  match Term.eval (runner_t, info) with
+  | `Error _ -> exit 1
+  | _ -> exit 0
 
 let () =
   Lwt_engine.set (new Lwt_engine.libev ());
