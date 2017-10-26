ca70e6b6d76c6f78e3b6c09cca1083b2daff1c65
Didier Wenzek <didier.wenzek@acidalie.com>
2017-10-26 16:12:49 +0200

    Files are assigned to their process using partition id.

--- examples/passover/TODO.md
+++ examples/passover/TODO.md
@@ -2,8 +2,6 @@
 * A message must be produced to the right partition.
 * Log parse errors
 * The most recent position event must be registered, no the last one. 
-* Each instance of a runner must have its own set of databases
-* Each instance of a runner must have its own set of state files
-* Each instance of a runner must have its own set of error files
 * Fix error: Kyoto.Error("opening the destination failed")
+* How can we ensure that exactly one proccess run for each partition ?
 * Implement an event generator.
--- examples/passover/passover.ml
+++ examples/passover/passover.ml
@@ -2,6 +2,7 @@ open Cmdliner
 open Message
 open Series
 open Util
+open Lwt.Infix
 module Dataset = Incremental_value
 
 let info =
@@ -28,8 +29,13 @@ let kafka_host =
   let doc = "Connection string to the Kafka cluster." in
   Arg.(value & opt string "localhost" & info ["k"; "kafka"] ~docv:"KAFKA_CLUSTER" ~doc)
 
+let working_dir =
+  let doc = "Directory where all data and states are stored." in
+  Arg.(value & opt string "/tmp/passover" & info ["d"; "data"] ~docv:"WORKING_DIR" ~doc)
+
 type cluster_info = {
   kafka_host: string;
+  working_dir: string;
   partition: int;
   partition_count: int;
 }
@@ -43,21 +49,48 @@ let produce_topic cluster topic =
   let host = cluster.kafka_host in
   Dataset.log (KafkaStore.Sink.log_to_topic ~host ~topic)
 
-let store_kyoto_plain =
+let prepare cluster =
+  Lwt.catch
+    (fun () ->
+      Lwt_unix.mkdir cluster.working_dir 0o750)
+    (function
+      | Unix.Unix_error(Unix.EEXIST,_,_) -> Lwt.return_unit
+      | e -> Lwt.fail e)
+
+let add_process_id cluster path =
+  let base = Filename.remove_extension path in
+  let ext = Filename.extension path in
+  let id = cluster.partition in
+  let count = cluster.partition_count in
+  Printf.sprintf "%s.%d-%d%s" base id count ext
+
+let add_working_dir cluster path =
+  if Filename.is_relative path
+  then Filename.concat cluster.working_dir path
+  else path
+
+let run cluster name =
+  let dir = cluster.working_dir in
+  let name = add_process_id cluster name in
+  Dataset.run dir name
+
+let store_kyoto_plain cluster =
   let encode_key = id in
   let decode_key = id in
   let encode_value = id in
   let decode_value = id in
   fun file_path ->
+    let file_path = file_path |> add_process_id cluster |> add_working_dir cluster in
     KyotoCabinet.KVStore.store ~encode_key ~decode_key ~encode_value ~decode_value ~file_path
     |> Dataset.persist_state
 
-let store_kyoto_string_int =
+let store_kyoto_string_int cluster =
   let encode_key = id in
   let decode_key = id in
   let encode_value = string_of_int in
   let decode_value = int_of_string in
   fun file_path ->
+    let file_path = file_path |> add_process_id cluster |> add_working_dir cluster in
     KyotoCabinet.KVStore.store ~encode_key ~decode_key ~encode_value ~decode_value ~file_path
     |> Dataset.persist_state
 
@@ -77,27 +110,29 @@ let decode_position_messages cluster =
   |> Dataset.map json_of_position
   |> Dataset.filter_map Option.of_result
   |> produce_topic cluster "passover.positions.events"  (* FIXME: should assign a partition *)
-  |> Dataset.run "/tmp/" "decode_position_messages"
+  |> run cluster "decode_position_messages"
 
 let update_current_visitor_room cluster =
   consume_topic cluster "passover.positions.events"
   |> Dataset.map position_of_json
   |> Dataset.filter_map Option.of_result
   |> Dataset.group visitor_of_event room_of_event most_recent
-  |> store_kyoto_plain "visitor_room.kct" 
-  |> Dataset.run "/tmp/" "update_current_visitor_room"
+  |> store_kyoto_plain cluster "visitor_room.kct" 
+  |> run cluster "update_current_visitor_room"
 
-let runner kafka_host partition partition_count =
+let runner kafka_host working_dir partition partition_count =
   let cluster =
-    { kafka_host; partition; partition_count; }
+    { kafka_host; working_dir; partition; partition_count; }
   in
-  Lwt_main.run (Lwt.join [
-    decode_position_messages cluster;
-    update_current_visitor_room cluster;
+  Lwt_main.run (
+    prepare cluster >>= fun () ->
+    Lwt.join [
+      decode_position_messages cluster;
+      update_current_visitor_room cluster;
   ])
 
 let runner_t =
-  Term.(pure runner $ kafka_host $ partition $ partition_count)
+  Term.(pure runner $ kafka_host $ working_dir $ partition $ partition_count)
 
 let main () =
   match Term.eval (runner_t, info) with
